3da1ee7d1e83074f61a7155134fc2ba64d513967
chloeke1379 <63062551+chloeke1379@users.noreply.github.com>
2022-07-08 18:54:07 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -50,7 +50,7 @@ const BLOG = {
       repo: ''
     },
     cusdisConfig: {
-      appId: '4e59b382-e9f8-4977-9551-7e5973cb7483', // data-app-id
+      appId: '0280f8f2-4841-4f5d-acfa-95232198b36a', // data-app-id
       host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
